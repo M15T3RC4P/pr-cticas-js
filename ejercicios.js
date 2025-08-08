493a6e826fcd225eceece492361d297e21ca9d96
Camilo <camilo42067@gmail.com>
2025-08-08 11:47:34 -0500

    primeros ejercicios

---
+++ ejercicios.js
@@ -0,0 +1,40 @@
+//mi solución Ejercicio 1
+let altura = parseInt(prompt('ingresa tu altura en metros:'));
+let peso = parseInt(prompt('Ingresa tu peso en kilogramos:'));
+
+function calculoMasaCorporal() {
+    let imc = peso / (altura * altura);
+    alert (`Su indice de masa corporal es ${imc}%`);
+}
+
+//como se debió solucionar Ejercicio 1
+
+function calcularMasaCorporal(altura, peso) {
+  let imc = peso / (altura * altura);
+  return imc;
+}
+// Ahora, los datos se obtienen fuera de la función
+let altura = parseFloat(prompt('Ingresa tu altura en metros:'));
+let peso = parseFloat(prompt('Ingresa tu peso en kilogramos:'));
+// Se llama a la función, pasándole los datos como argumentos
+let resultadoIMC = calcularMasaCorporal(altura, peso);
+// Se muestra el resultado
+alert(`Su índice de masa corporal es ${resultadoIMC}`);
+
+//Mi solución Ejercicio 2
+
+function calculoFactorial(numero) {
+    let resultado = 1;
+    for ( let i = 1; i<=numero;i++) {
+        resultado = resultado * i;
+    }  
+    return resultado;
+}
+
+//Mi solución Ejercicio 3
+
+function convertidorDivisas(cantidadCop) {
+    let valorUsd = 0.00025;
+    let calculoDivisas = cantidadCop * valorUsd;
+    return calculoDivisas
+}
